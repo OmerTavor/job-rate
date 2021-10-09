198914b506c9d4909b3fada7e3b542b6ceb0fb1d
OmerTavor <58879865+OmerTavor@users.noreply.github.com>
2021-10-09 18:21:29 +0300

    Adding routing to the web app
    Added React Router to the app.
    Connected the router through the main App component.
    Added 2 pages to the app and connected them to the router:
    1) Home page (HomePage component)
    2) View page (ViewPage component)
    Both pages are on a basic stage for now.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ViewPage from './pages/ViewPage';
 
 const App = () => {
   return (
     <div className="defaultAppPage">
-      <HomePage />
+      <Switch>
+        <Route path="/view" exact>
+          <ViewPage />
+        </Route>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        {/* <Route path="/*" exact>
+          <h1>GGG</h1>
+        </Route> */}
+      </Switch>
     </div>
   );
 };
