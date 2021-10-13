6fa1b03da0560bb0af37235e0a12d56c11266dbb
OmerTavor <58879865+OmerTavor@users.noreply.github.com>
2021-10-13 03:25:42 +0300

    Seperating colorScale
    Seperating the colorScale to an independent file.
    defining new edges to the colorScale - 1 to 7 .

---
+++ src/assets/colorScale.js
@@ -0,0 +1,11 @@
+import ColorScale from 'color-scales';
+
+const redYellowGreenColorScale = new ColorScale(1, 7, [
+  '#ff0000',
+  '#ff8000',
+  '#ffff00',
+  '#80ff00',
+  '#00CC00',
+]);
+
+export { redYellowGreenColorScale };
