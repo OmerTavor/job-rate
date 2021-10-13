31630dd4e6194ee915db1d6307846f0740587cf8
OmerTavor <58879865+OmerTavor@users.noreply.github.com>
2021-10-13 03:24:21 +0300

    Building dynamic data
    Building dynamic data that wll help in the server later - questionsBank.js & jobQA.js .

---
+++ src/data/jobQA.js
@@ -0,0 +1,57 @@
+const jobQA = {
+  id: '578397432',
+  companyName: 'Google',
+  finalGrade: 6.2,
+  QA: [
+    {
+      questionId: '1',
+      isGolden: false,
+      grade: 7,
+      note: '35,000 NIS',
+    },
+    {
+      questionId: '2',
+      isGolden: true,
+      grade: 6,
+      note: 'Back end developr of Google Glasses',
+    },
+    {
+      questionId: '4',
+      isGolden: false,
+      grade: 6.5,
+      note: 'Top technical people in the country',
+    },
+    {
+      questionId: '5',
+      isGolden: true,
+      grade: 6.5,
+      note: 'Great people to learn from',
+    },
+    {
+      questionId: '7',
+      isGolden: true,
+      grade: 4,
+      note: 'have some restrictions but not awful',
+    },
+    {
+      questionId: '8',
+      isGolden: true,
+      grade: 5,
+      note: 'mix of young and experinced',
+    },
+    {
+      questionId: '10',
+      isGolden: false,
+      grade: 6.8,
+      note: '',
+    },
+    {
+      questionId: '14',
+      isGolden: false,
+      grade: 4,
+      note: 'Google is huge',
+    },
+  ],
+};
+
+module.exports = { jobQA };
---
+++ src/data/questionBank.js
@@ -0,0 +1,75 @@
+const questionsBank = [
+  {
+    questionId: '1',
+    questionDetail: 'Salary offer meet my requirements ?',
+  },
+  {
+    questionId: '2',
+    questionDetail: 'The job described is a interesting role for me ?',
+  },
+  {
+    questionId: '3',
+    questionDetail: 'The job described have great weight in the company ?',
+  },
+  {
+    questionId: '4',
+    questionDetail:
+      'Employees with quality technical experience that I can learn from ?',
+  },
+  {
+    questionId: '5',
+    questionDetail:
+      'Employees with quality management experience that I can learn from ?',
+  },
+  {
+    questionId: '6',
+    questionDetail:
+      'Position relevant to the technologies in which I specialize ?',
+  },
+  {
+    questionId: '7',
+    questionDetail:
+      'High level of flexibility at work (home / office, arrival / departure) ?',
+  },
+  {
+    questionId: '8',
+    questionDetail: 'Employees in the company mostly young / young vibe ?',
+  },
+  {
+    questionId: '9',
+    questionDetail: 'Workplace in a convenient location for me ?',
+  },
+  {
+    questionId: '10',
+    questionDetail:
+      'Is the proposed contract an attractive contract (shares / signing bonus / options) ?',
+  },
+  {
+    questionId: '11',
+    questionDetail: 'Is the physical workspace pleasant to stay in ?',
+  },
+  {
+    questionId: '12',
+    questionDetail: 'Does the company pamper the employees (gifts / events) ?',
+  },
+  {
+    questionId: '13',
+    questionDetail:
+      'Do I connect to the idea / product / vision of the company ?',
+  },
+  {
+    questionId: '14',
+    questionDetail: 'The size of the R&D department is the ideal size for me ?',
+  },
+  {
+    questionId: '15',
+    questionDetail: 'Level of the the organizational culture ?',
+  },
+  {
+    questionId: '16',
+    questionDetail:
+      'Is the interview process purposeful (important or bland) ?',
+  },
+];
+
+module.exports = { questionsBank };
